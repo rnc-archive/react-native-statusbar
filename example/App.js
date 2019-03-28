40774db9767bf834b3264af25cc8b83a2c77dcc7
Dmitriy Kovalenko <dmtr.kovalenko@outlook.com>
2019-03-28 15:55:36 +0200

    Refactor example App.js

--- example/App.js
+++ example/App.js
@@ -7,75 +7,68 @@
  * @lint-ignore-every XPLATJSCOPYRIGHT1
  */
 
-import React, {Component} from 'react';
-import {StyleSheet, Text, View, TouchableHighlight} from 'react-native';
+import React from 'react';
 import StatusBar from '@react-native-community/status-bar';
+import {StyleSheet, Text, View, TouchableHighlight} from 'react-native';
 
-type Props = {};
+type MutationBtnProps = {
+  label: string,
+  testID: string,
+  onPress: () => void,
+};
 
-export default class App extends Component<Props> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <TouchableHighlight
-          testID="set-hidden-slide"
-          style={styles.wrapper}
-          onPress={() => {
-            StatusBar.setHidden(true, 'slide');
-          }}>
-          <View style={styles.button}>
-            <Text>setHidden(true, 'slide')</Text>
-          </View>
-        </TouchableHighlight>
-        <TouchableHighlight
-          style={styles.wrapper}
-          onPress={() => {
-            StatusBar.setHidden(false, 'fade');
-          }}>
-          <View style={styles.button}>
-            <Text>setHidden(false, 'fade')</Text>
-          </View>
-        </TouchableHighlight>
-        <TouchableHighlight
-          style={styles.wrapper}
-          onPress={() => {
-            StatusBar.setBarStyle('default', true);
-          }}>
-          <View style={styles.button}>
-            <Text>setBarStyle('default', true)</Text>
-          </View>
-        </TouchableHighlight>
-        <TouchableHighlight
-          style={styles.wrapper}
-          onPress={() => {
-            StatusBar.setBarStyle('light-content', true);
-          }}>
-          <View style={styles.button}>
-            <Text>setBarStyle('light-content', true)</Text>
-          </View>
-        </TouchableHighlight>
-        <TouchableHighlight
-          style={styles.wrapper}
-          onPress={() => {
-            StatusBar.setNetworkActivityIndicatorVisible(true);
-          }}>
-          <View style={styles.button}>
-            <Text>setNetworkActivityIndicatorVisible(true)</Text>
-          </View>
-        </TouchableHighlight>
-        <TouchableHighlight
-          style={styles.wrapper}
-          onPress={() => {
-            StatusBar.setNetworkActivityIndicatorVisible(false);
-          }}>
-          <View style={styles.button}>
-            <Text>setNetworkActivityIndicatorVisible(false)</Text>
-          </View>
-        </TouchableHighlight>
+const MutationBtn = ({label, onPress, testID}: MutationBtnProps) => {
+  return (
+    <TouchableHighlight
+      testID="set-hidden-slide"
+      style={styles.wrapper}
+      onPress={onPress}>
+      <View style={styles.button}>
+        <Text>{label}</Text>
       </View>
-    );
-  }
-}
+    </TouchableHighlight>
+  );
+};
+
+const App = () => (
+  <View style={styles.container}>
+    <MutationBtn
+      testID="set-hidden-slide"
+      label="setHidden(true, 'slide')"
+      onPress={() => StatusBar.setHidden(true, 'slide')}
+    />
+
+    <MutationBtn
+      testID="set-visible-fade"
+      label="setHidden(false, 'fade')"
+      onPress={() => StatusBar.setHidden(false, 'fade')}
+    />
+
+    <MutationBtn
+      testID="set-default-style"
+      label="setBarStyle('default', true)"
+      onPress={() => StatusBar.setBarStyle('default', true)}
+    />
+
+    <MutationBtn
+      testID="set-light-style"
+      label="setBarStyle('light-content', true)"
+      onPress={() => StatusBar.setBarStyle('light-content', true)}
+    />
+
+    <MutationBtn
+      testID="set-network-hidden"
+      label="setNetworkActivityIndicatorVisible(true)"
+      onPress={() => StatusBar.setNetworkActivityIndicatorVisible(true)}
+    />
+
+    <MutationBtn
+      testID="set-network-visible"
+      label="setNetworkActivityIndicatorVisible(false)"
+      onPress={() => StatusBar.setNetworkActivityIndicatorVisible(false)}
+    />
+  </View>
+);
 
 const styles = StyleSheet.create({
   container: {
@@ -102,3 +95,5 @@ const styles = StyleSheet.create({
     marginTop: 10,
   },
 });
+
+export default App;
